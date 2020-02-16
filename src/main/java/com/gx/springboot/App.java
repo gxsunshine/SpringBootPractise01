c74e2c49934580c4e4bcf8a7f3d1cc1d18dcf3d1
gxsunshine <375870381@qq.com>
2020-02-16 11:04:35 +0800

    modify .gitignore

--- src/main/java/com/gx/springboot/App.java
+++ src/main/java/com/gx/springboot/App.java
@@ -3,6 +3,11 @@ package com.gx.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * sprintboot test
+ * @author gx
+ * @Date 2020年2月16日 上午10:53:12
+ */
 @SpringBootApplication
 public class App {
 
